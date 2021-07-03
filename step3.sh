3981f5017817d7ab39bef88141134ace672fbef7
utsocials <utsocials@gmail.com>
2021-07-03 01:36:51 -0700

    without vpn

--- step3.sh
+++ step3.sh
@@ -6,8 +6,8 @@ rc-update add dbus
 rc-service lightdm start
 rc-update add lightdm
 
-rc-service open-vm-tools start
-rc-update add open-vm-tools boot
+#rc-service open-vm-tools start
+#rc-update add open-vm-tools boot
 
 rc-service docker start
 rc-update add docker boot
