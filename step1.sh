eec31a587ecbbbbc2e7d6173b0eae64265924f0d
utsocials <utsocials@gmail.com>
2021-07-03 01:29:20 -0700

    adding new repository

--- step1.sh
+++ step1.sh
@@ -1,6 +1,7 @@
 #!/bin/ash
 
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.13/community" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
 apk update
 apk upgrade
 
