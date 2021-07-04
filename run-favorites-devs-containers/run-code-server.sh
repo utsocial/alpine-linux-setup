d721d2503c5a8996c4269a06e46d7d055d5a626a
Linux User <ibuetler@alpine.my.domain>
2021-07-03 19:13:48 -0700

    adding favorites dev containers

---
+++ run-favorites-devs-containers/run-code-server.sh
@@ -0,0 +1,14 @@
+docker run -d \
+  --name=code-server \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/London \
+  -e PASSWORD=password `#optional` \
+  -e HASHED_PASSWORD= `#optional` \
+  -e SUDO_PASSWORD=password `#optional` \
+  -e SUDO_PASSWORD_HASH= `#optional` \
+  -e PROXY_DOMAIN=code-server.my.domain `#optional` \
+  -p 8443:8443 \
+  -v /home/ibuetler/code-server/config:/config \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/code-server
