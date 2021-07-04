4ce4ed58bfbab1518f37110acb6a3b8511f8f10d
utsocials <utsocials@gmail.com>
2021-07-03 21:28:06 -0700

    remove user to cheno and headless

--- step2.sh
+++ step2.sh
@@ -1,48 +1,25 @@
 #!/bin/ash
+apk add curl socat nmap net-tools build-base setxkbmap sudo bash
 
-apk add curl socat nmap net-tools build-base setxkbmap sudo xrandr bash termite zsh dbus dbus-x11
-setup-xorg-base xfce4 xfce4-terminal lightdm dbus-x11
-apk add open-vm-tools open-vm-tools-guestinfo open-vm-tools-deploypkg open-vm-tools-gtk
-apk add lightdm-gtk-greeter i3wm i3status libxcb-dev i3lock xf86-video-vmware dmenu
-apk add mesa-gl glib feh firefox-esr accountsservice 
-# apk add openvpn
 apk add docker docker-compose
 
 # add user
-adduser ibuetler
-mkdir -p /home/ibuetler/wallpaper
-mkdir -p /home/ibuetler/.config/i3
+adduser cheno
 
-# user setup ibuetler
-cp ./ibuetler/wallpaper/compass.jpg /home/ibuetler/wallpaper/compass.jpg
-cp ./ibuetler/.config/i3/config /home/ibuetler/.config/i3/config
-cp ./ibuetler/.profile /home/ibuetler/.profile
-mkdir -p /home/ibuetler/.scripts
-cp ./ibuetler/login-script.sh /home/ibuetler/.scripts/login-script.sh
-chown -R ibuetler:ibuetler /home/ibuetler
+# user setup cheno
+cp ./cheno/.profile /home/cheno/.profile
+chown -R cheno:cheno /home/cheno
 
-# add ibuetler to sudoers
-cat ./ibuetler/sudoers >> /etc/sudoers
-
-# greeter background
-echo "background=/home/ibuetler/wallpaper/compass.jpg" >> /etc/lightdm/lightdm-gtk-greeter.conf
-
-# set background image in accountsservice
-cp ./ibuetler/ibuetler /var/lib/AccountsService/users
-chown root:root /var/lib/AccountsService/users/ibuetler
+# add cheno to sudoers
+cat ./cheno/sudoers >> /etc/cheno
 
 # add user to docker
-addgroup ibuetler docker
+addgroup cheno docker
 
-# enable copy paste in vmware
-chmod g+s /usr/bin/vmware-user-suid-wrapper
-
-# give ibuetler write access to /opt dir
-chown ibuetler:ibuetler /opt
+# give cheno write access to /opt dir
+chown cheno:cheno /opt
 
 # mkdir /opt/docker
 mkdir -p /opt/docker
 cp ./docker/* /opt/docker/
-chown ibuetler:ibuetler /opt/docker
-
-
+chown cheno:cheno /opt/docker
