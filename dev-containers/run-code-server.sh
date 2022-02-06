3615f552960bf5e9d65b33f5ac0221e56e99299a
Linux User <noroot@alpine.my.domain>
2022-02-06 00:27:23 -0700

    fix container user instruction

--- dev-containers/run-code-server.sh
+++ dev-containers/run-code-server.sh
@@ -9,6 +9,6 @@ docker run -d \
   -e SUDO_PASSWORD_HASH= `#optional` \
   -e PROXY_DOMAIN=code-server.my.domain `#optional` \
   -p 8443:8443 \
-  -v /home/cheno/code:/config \
+  -v /home/noroot/code:/config \
   --restart unless-stopped \
   ghcr.io/linuxserver/code-server
