14833ad55c97401fff0533e110abec1033936f8b
Ivan R. Chenoweth <ivanchenoweth@gmail.com>
2022-05-23 22:30:11 -0700

    add community script

---
+++ stepcommunity.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+
+cat > /etc/apk/repositories << EOF; $(echo)
+
+https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/main/
+https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community/
+https://dl-cdn.alpinelinux.org/alpine/edge/testing/
+
+EOF
